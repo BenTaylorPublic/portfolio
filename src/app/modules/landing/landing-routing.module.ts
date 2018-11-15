592e8a3cd9bf9f798e0bb69dfe906d0579d03d76
Ben Taylor <benjaminjacktaylor@hotmail.com>
2018-11-15 21:45:04 +1100

    Removed extra comma

--- src/app/modules/landing/landing-routing.module.ts
+++ src/app/modules/landing/landing-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { LandingComponent } from "./components/landing/landing.component";
 
 const routes: Routes = [
-    { path: "", component: LandingComponent },
+    { path: "", component: LandingComponent }
 ];
 
 @NgModule({
