6d37a49dbec234f5b3031a88e76c29dfd9694211
Ben Taylor <benjaminjacktaylor@hotmail.com>
2018-11-25 17:22:51 +1100

    Removed bentaylor

--- src/app/modules/projects/components/projects/projects.component.ts
+++ src/app/modules/projects/components/projects/projects.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 
+export enum ProjectEnum {
+    OneFunction = 0,
+    Coldsteel = 1
+}
+
 @Component({
     selector: "app-projects",
     templateUrl: "./projects.component.html",
     styleUrls: ["./projects.component.scss"]
 })
 export class ProjectsComponent implements OnInit {
+
+    public currentTab: ProjectEnum = ProjectEnum.OneFunction;
 
     constructor() { }
 
--- src/app/modules/projects/projects.module.ts
+++ src/app/modules/projects/projects.module.ts
@@ -5,7 +5,6 @@ import { ProjectsRoutingModule } from "./projects-routing.module";
 import { ProjectsComponent } from "./components/projects/projects.component";
 import { OneFunctionComponent } from "./components/one-function/one-function.component";
 import { ColdsteelComponent } from "./components/coldsteel/coldsteel.component";
-import { BenTaylorComponent } from "./components/ben-taylor/ben-taylor.component";
 
 @NgModule({
     imports: [
@@ -15,8 +14,7 @@ import { BenTaylorComponent } from "./components/ben-taylor/ben-taylor.component
     declarations: [
         ProjectsComponent,
         OneFunctionComponent,
-        ColdsteelComponent,
-        BenTaylorComponent
+        ColdsteelComponent
     ]
 })
 export class ProjectsModule { }
