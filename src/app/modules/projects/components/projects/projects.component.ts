b723e07b20fb5c069438c201a84c9165e1093d44
Ben Taylor <benjaminjacktaylor@hotmail.com>
2018-12-25 22:36:21 +1100

    Implemented the card approach of projects

--- src/app/modules/projects/components/projects/projects.component.ts
+++ src/app/modules/projects/components/projects/projects.component.ts
@@ -12,4 +12,14 @@ export class ProjectsComponent implements OnInit {
     ngOnInit() {
     }
 
+    public projectCardClicked(projectName: string): void {
+        if (projectName === "onefunction") {
+            window.open("https://github.com/BenTaylorPublic/one-function", "_blank");
+        } else if (projectName === "coldsteel") {
+            alert("Coldsteel isn't currently on Github, but check back soon!");
+        } else if (projectName === "bentaylor") {
+            window.open("https://github.com/BenTaylorPublic/bentaylorsrc", "_blank");
+        }
+    }
+
 }
