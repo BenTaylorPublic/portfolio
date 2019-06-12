4237d2fb7851ef27e9adf1ca4684d26beb84e85c
Ben <benjaminjacktaylor@hotmail.com>
2019-06-12 12:38:04 +1000

    Fixed the projects screen
    removed bentaylor
    renamed coldsteel to ipoc
    added screeps

--- src/app/modules/projects/components/projects/projects.component.ts
+++ src/app/modules/projects/components/projects/projects.component.ts
@@ -17,10 +17,10 @@ export class ProjectsComponent implements OnInit {
     public projectCardClicked(projectName: string): void {
         if (projectName === "onefunction") {
             window.open("https://github.com/BenTaylorPublic/one-function", "_blank");
-        } else if (projectName === "coldsteel") {
-            alert("Coldsteel isn't currently on Github, but check back soon!");
-        } else if (projectName === "bentaylor") {
-            window.open("https://github.com/BenTaylorPublic/bentaylorsrc", "_blank");
+        } else if (projectName === "ipoc") {
+            alert("IPOC isn't currently on Github, but check back soon!");
+        } else if (projectName === "screeps") {
+            window.open("https://github.com/BenTaylorPublic/screeps", "_blank");
         }
     }
 
