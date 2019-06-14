ca91cfdc9f8a982330af553983c4cedff694c16f
Ben <benjaminjacktaylor@hotmail.com>
2019-06-14 12:16:46 +1000

    Added eve-dota-mmo component

---
+++ src/app/modules/game-design/components/eve-dota-mmo/eve-dota-mmo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-eve-dota-mmo",
+    templateUrl: "./eve-dota-mmo.component.html",
+    styleUrls: ["./eve-dota-mmo.component.scss"]
+})
+export class EveDotaMmoComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
--- src/app/modules/game-design/game-design.module.ts
+++ src/app/modules/game-design/game-design.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from "@angular/common";
 
 import { GameDesignRoutingModule } from "./game-design-routing.module";
 import { GameDesignComponent } from "./components/game-design/game-design.component";
+import { EveDotaMmoComponent } from "./components/eve-dota-mmo/eve-dota-mmo.component";
 
 @NgModule({
     imports: [
         CommonModule,
         GameDesignRoutingModule
     ],
-    declarations: [GameDesignComponent]
+    declarations: [GameDesignComponent, EveDotaMmoComponent]
 })
 export class GameDesignModule {
 }
