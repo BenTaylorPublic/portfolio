538742c9f7d5146563aff54611e6d6ef24944bbe
Ben <benjaminjacktaylor@hotmail.com>
2019-06-20 13:04:59 +1000

    Version bump and release to v0.1.5

--- src/app/modules/app/services/app/app.service.ts
+++ src/app/modules/app/services/app/app.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from "@angular/core";
 })
 export class AppService {
 
-    private appVersion: string = "0.1.4";
+    private appVersion: string = "0.1.5";
 
     constructor() {
         console.info("App version v" + this.appVersion);
