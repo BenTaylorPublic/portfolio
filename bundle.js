55bead3a8fd9d5c4fa7977aaced83571c36a2feb
ben <benjaminjacktaylor@hotmail.com>
2019-11-11 16:30:23 +1100

    Got the build process working with browserify

---
+++ bundle.js
@@ -0,0 +1,18 @@
+console.log("Bundling...");
+const environment = process.argv.slice(2)[0];
+
+console.log("Environment: " + environment);
+
+let directory = "";
+if (environment === "prod") {
+    directory = "docs";
+} else if (environment === "dev") {
+    directory = "src";
+}
+
+const browserify = require("browserify");
+const fs = require("fs");
+
+let b = browserify();
+b.add("./" + directory + "/script.js");
+b.bundle().pipe(fs.createWriteStream("./" + directory + "/bundle.js"));
---
+++ docs/some-class.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class MyCoolClass {
+    static hello() {
+        console.log("AYY");
+    }
+}
+exports.MyCoolClass = MyCoolClass;
