4c67475601d646be0f8fd94e8962c34a22c6cfe3
ben <benjaminjacktaylor@hotmail.com>
2019-11-14 08:17:27 +1100

    Finished my build logic fixing the relative strings

--- bundle-script.js
+++ bundle-script.js
@@ -37,23 +37,42 @@ const browserify = require("browserify");
 const fs = require("fs");
 
 for (const bundle of bundles) {
+    const outputFile = "./" + outDir + bundle.entryDir + bundle.name + ".bundle.js";
+
     let b = browserify();
-    console.log("Bundling " + bundle.name);
-    b.add("./tsc-dist/" + bundle.entryDir + bundle.entry, {
-        basedir: "./"
-    });
+    b.add("./tsc-dist/" + bundle.entryDir + bundle.entry);
     for (const sharedJs of shared) {
         b.exclude("./tsc-dist/" + sharedJs.entryDir + sharedJs.entry);
     }
-    b.bundle().pipe(fs.createWriteStream("./" + outDir + bundle.entryDir + bundle.name + ".bundle.js"));
+    b.bundle(function (err1, buf) {
+
+        if (err1) {
+            console.log(err1);
+            return;
+        }
+
+        /*
+        Manually manipulating the javascript
+        This is because in the dom it's a flat structure, but Browserify requires still have the relative layout
+        The only thing it needs to "require" is shared stuff, so it should be fine to replace fix the path
+        */
+        let bundleAsString = buf.toString();
+        bundleAsString = bundleAsString.split(`"../../../`).join(`"./`);
+        bundleAsString = bundleAsString.split(`"../../`).join(`"./`);
+        bundleAsString = bundleAsString.split(`"../`).join(`"./`);
+
+        fs.writeFile(outputFile, bundleAsString, function (err2) {
+            if (err2) {
+                return console.log(err2);
+            }
+        });
+    });
 }
 
 let b = browserify();
-console.log("Bundling shared");
 for (const sharedJs of shared) {
     b.require("./tsc-dist/" + sharedJs.entryDir + sharedJs.entry, {
-        expose: sharedJs.expose,
-        basedir: "./"
+        expose: sharedJs.expose
     });
 }
 b.bundle().pipe(fs.createWriteStream("./" + outDir + "shared.bundle.js"));
