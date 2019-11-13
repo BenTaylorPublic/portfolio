a3bce37530a8a8e6a54067d6a8c4ec7eda94b9fc
ben <benjaminjacktaylor@hotmail.com>
2019-11-13 16:06:02 +1100

    Changed the bundle script to have an array of bundles to iterate

--- bundle-script.js
+++ bundle-script.js
@@ -1,22 +1,36 @@
 console.log("Bundling...");
+
+const bundles = [
+    {
+        name: "script",
+        entryDir: "",
+        entry: "script.js"
+    },
+    {
+        name: "example",
+        entryDir: "modules/example-module/",
+        entry: "example.js"
+    }
+];
+
+
 const environment = process.argv.slice(2)[0];
 
 console.log("Environment: " + environment);
 
-let directory = "";
+let outDir = "";
 if (environment === "prod") {
-    directory = "docs";
+    outDir = "docs/";
 } else if (environment === "dev") {
-    directory = "src";
+    outDir = "src/";
 }
 
 const browserify = require("browserify");
 const fs = require("fs");
 
-let b = browserify();
-b.add("./tsc-dist/script.js");
-b.bundle().pipe(fs.createWriteStream("./" + directory + "/script.bundle.js"));
-
-b = browserify();
-b.add("./tsc-dist/modules/example-module/example.js");
-b.bundle().pipe(fs.createWriteStream("./" + directory + "/modules/example-module/example.bundle.js"));
+for (const bundle of bundles) {
+    let b = browserify();
+    console.log("Bundling " + bundle.name);
+    b.add("./tsc-dist/" + bundle.entryDir + bundle.entry);
+    b.bundle().pipe(fs.createWriteStream("./" + outDir + bundle.entryDir + bundle.name + ".bundle.js"));
+}
