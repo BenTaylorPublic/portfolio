b7b76bdbc0f0b2101aec6ba2f4175f3e44f74f3b
ben <benjaminjacktaylor@hotmail.com>
2019-11-16 14:46:59 +1100

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 class IndexView {
     public static initialize(): void {
-        const button: HTMLElement = document.getElementById("btnProjects");
-        button.addEventListener("click", () => this.projectsClicked());
-    }
-
-    public static projectsClicked(): void {
-        alert("Back soon!");
     }
 }
 
