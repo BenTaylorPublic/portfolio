22a57367519d2da7aecf95d6007e1789b56bf0cd
ben <benjaminjacktaylor@hotmail.com>
2020-05-10 19:09:15 +1000

    Fixed the animations via js

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 class IndexView {
     public static initialize(): void {
+        //In a timeout to not trigger the animation on first load
+        //Maybe not the best but it works for now
+        setTimeout(() => {
+            const projectCardChilds: HTMLCollectionOf<Element> = document.getElementsByClassName("projectCardChild");
+            for (let i: number = 0; i < projectCardChilds.length; i++) {
+                projectCardChilds[i].classList.remove("preload");
+            }
+        }, 200);
     }
 }
 
